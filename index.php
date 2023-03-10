1f0bbf3fda850146fb5150df5e4f7d6e793fc97f
CarloColletti~ <carlocolletti5@gmail.com>
2023-03-10 21:55:49 +0100

    create le card hotel senza personalizzazione

--- index.php
+++ index.php
@@ -62,9 +62,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body>  
+    <h1>
+        Lista Hotels:
+    </h1>
+    
+    <div class="container">
+        <div class="row row-cols-5">
+            <?php foreach($hotels as $hotel_card) { ?>
+                <div class="col d-flex flex-column">
+                    <?php foreach($hotel_card as $hotel) { ?>
+                        <span><?= $hotel ?></span>
+                    <?php } ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+
     
 </body>
 </html>
