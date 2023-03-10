54b0442b5c96a41964d1728c64ae1a53bcac1b4d
CarloColletti~ <carlocolletti5@gmail.com>
2023-03-10 22:29:36 +0100

    personalizzazione

--- index.php
+++ index.php
@@ -62,8 +62,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- foglio di stile  -->
+    <link rel="stylesheet" href="./style.css">
+
     <title>Document</title>
 </head>
 <body>  
@@ -72,11 +75,19 @@
     </h1>
     
     <div class="container">
-        <div class="row row-cols-5">
+        <div class="row">
+            <div class="col-12">
+                <form method="get" action="./greetings.php"></form>
+            </div>
+        </div>
+
+
+
+        <div class="row row-cols-4 gap-2 justify-content-center">
             <?php foreach($hotels as $hotel_card) { ?>
-                <div class="col d-flex flex-column">
+                <div class="col d-flex flex-column card-p">
                     <?php foreach($hotel_card as $hotel) { ?>
-                        <span><?= $hotel ?></span>
+                        <span class="py-2"><?= $hotel ?></span>
                     <?php } ?>
                 </div>
             <?php } ?>
